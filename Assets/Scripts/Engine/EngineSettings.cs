e7e34e12ef46d7a227af94f0e7ff43f9df67c86a
kms <marlon12@naver.com>
2024-06-14 22:25:51 +0900

    Settings changes

--- Assets/Scripts/Engine/EngineSettings.cs
+++ Assets/Scripts/Engine/EngineSettings.cs
@@ -4,7 +4,7 @@ public static class EngineSettings
     public static bool enableBlackEngine = false;
     public static bool useThreading = true;
     public static int searchDepth = 8;
-    public static int searchMs = 15000;
+    public static int searchMs = 3000;
     public static int ttSize = 64000;
 
 
